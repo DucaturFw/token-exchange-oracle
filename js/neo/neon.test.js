92ae357a790ec100ab3719452a7c08d51e9fc50f
zlumer <zlumer@gmail.com>
2018-06-27 20:24:44 +0300

    neo: more neon-js tests

--- js/neo/neon.test.js
+++ js/neo/neon.test.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var neon_js_1 = require("@cityofzion/neon-js");
+var neon_js_1 = __importDefault(require("@cityofzion/neon-js"));
+var config_1 = __importDefault(require("../config"));
 it('should parse hex string', function () {
     expect(neon_js_1.u.hexstring2str("656f73")).toEqual("eos");
 });
@@ -15,4 +19,25 @@ it('should get address from script hash', function () {
 });
 it('should convert fixed8', function () {
     expect(neon_js_1.u.fixed82num("404b4c")).toEqual(0.05);
+});
+it('should create account', function () {
+    var acc = neon_js_1.default.create.account("AKQ8cCUoE99ncnRRbaYPit3pV3g58A6FJk");
+    expect(acc).toBeDefined();
+    expect(acc.scriptHash).toEqual("f149f74cf5fa1d61c11a5b1a90ccb7871ad7c327");
+    // expect(acc.publicKey).toEqual('0200da59142c4ce8414091cafe35b063335c8536c9d3c11a56216910179cfec2b2')
+});
+it('should parse PK to address', function () {
+    var acc = neon_js_1.default.create.account(config_1.default.neo.contract.owner_pk);
+    expect(acc).toBeDefined();
+    expect(acc.address).toEqual('AKQ8cCUoE99ncnRRbaYPit3pV3g58A6FJk');
+    expect(acc.scriptHash).toEqual('f149f74cf5fa1d61c11a5b1a90ccb7871ad7c327');
+    expect(acc.publicKey).toEqual('0200da59142c4ce8414091cafe35b063335c8536c9d3c11a56216910179cfec2b2');
+});
+var DEFAULT_RPC = {
+    MAIN: 'http://seed2.neo.org:10332',
+    TEST: 'http://seed2.neo.org:20332'
+};
+it('should connect to rpc', function (done) {
+    var q = neon_js_1.default.create.query({ method: "getblockcount" });
+    q.execute(config_1.default.neo.rpc).then(function (x) { return ( /* console.log(x),  */done()); }).catch(done);
 });
--- src/neo/neon.test.ts
+++ src/neo/neon.test.ts
@@ -1,4 +1,5 @@
-import { u as neon_u, wallet } from "@cityofzion/neon-js"
+import Neon, { u as neon_u, wallet } from "@cityofzion/neon-js"
+import appConfig from "../config"
 
 it('should parse hex string', () =>
 {
@@ -18,4 +19,28 @@ it('should get address from script hash', () =>
 it('should convert fixed8', () =>
 {
 	expect(neon_u.fixed82num("404b4c")).toEqual(0.05)
+})
+it('should create account', () =>
+{
+	let acc = Neon.create.account("AKQ8cCUoE99ncnRRbaYPit3pV3g58A6FJk")
+	expect(acc).toBeDefined()
+	expect(acc.scriptHash).toEqual("f149f74cf5fa1d61c11a5b1a90ccb7871ad7c327")
+	// expect(acc.publicKey).toEqual('0200da59142c4ce8414091cafe35b063335c8536c9d3c11a56216910179cfec2b2')
+})
+it('should parse PK to address', () =>
+{
+	let acc = Neon.create.account(appConfig.neo.contract.owner_pk)
+	expect(acc).toBeDefined()
+	expect(acc.address).toEqual('AKQ8cCUoE99ncnRRbaYPit3pV3g58A6FJk')
+	expect(acc.scriptHash).toEqual('f149f74cf5fa1d61c11a5b1a90ccb7871ad7c327')
+	expect(acc.publicKey).toEqual('0200da59142c4ce8414091cafe35b063335c8536c9d3c11a56216910179cfec2b2')
+})
+let DEFAULT_RPC = {
+	MAIN: 'http://seed2.neo.org:10332',
+	TEST: 'http://seed2.neo.org:20332'
+  }
+it('should connect to rpc', done =>
+{
+	let q = Neon.create.query({ method:"getblockcount" })
+	q.execute(appConfig.neo.rpc).then(x => (/* console.log(x),  */done())).catch(done)
 })
