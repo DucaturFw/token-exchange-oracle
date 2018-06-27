e99a7cd6ba7ddc86fed196b7e1a966ad2189af3b
zlumer <zlumer@gmail.com>
2018-06-27 20:28:38 +0300

    neo: sending neo

--- js/neo/neo.js
+++ js/neo/neo.js
@@ -3,9 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var neon_js_1 = __importDefault(require("@cityofzion/neon-js"));
 var config_1 = __importDefault(require("../config"));
 var lib_1 = require("./lib");
 var neo_vm_1 = require("./neo-vm");
+var tools_1 = require("./tools");
 exports.NEO_CONTRACT_ADDR = config_1.default.neo.contract.addr;
 function getNeoTransfers(callback) {
     lib_1.get_last_transactions_by_address(exports.NEO_CONTRACT_ADDR, function (err, txs) {
@@ -25,6 +27,8 @@ function getNeoTransfers(callback) {
                 var contract = neo_vm_1.parseExchangeCall(tx.script);
                 if (!contract || contract.method != "exchange")
                     return;
+                if (!log)
+                    return console.log("skipping tx #" + tx.txid + " without applog!");
                 if (!neo_vm_1.checkTxSuccess(log)) // failed transaction (shouldn't be in the blockchain anyway)
                     return;
                 var _b = contract.params, from = _b[0], amount = _b[1], blockchainTo = _b[2], to = _b[3];
@@ -49,5 +53,35 @@ function sendNeoToken(transfer) {
     console.log("\n\n-----TRANSFER NEO-----\n");
     console.log(transfer);
     console.log("\n----------------------\n\n");
+    var neoscan = config_1.default.neo.apiUrl + "/api/main_net";
+    var ctrHash = config_1.default.neo.contract.hash;
+    var makeScript = tools_1.toScriptString(ctrHash);
+    var callContract = tools_1.ctrInvoker(ctrHash, config_1.default.neo.rpc);
+    var to = neon_js_1.default.create.account(transfer.to);
+    console.log(transfer.to);
+    console.log(to.scriptHash);
+    callContract = function (op) {
+        var args = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            args[_i - 1] = arguments[_i];
+        }
+        return tools_1.invoke(tools_1.signRawTx(config_1.default.neo.contract.owner_pk, makeScript(op, args)).serialize(true), config_1.default.neo.rpc);
+    };
+    function sendTx(script) {
+        return neon_js_1.default.doInvoke({
+            net: neoscan,
+            url: config_1.default.neo.rpc,
+            script: script,
+            account: neon_js_1.default.create.account(config_1.default.neo.contract.owner_pk),
+            gas: 0.001,
+        }).then(function (tx) { return tx; });
+    }
+    sendTx(makeScript('mintTokens', neon_js_1.u.reverseHex(to.scriptHash), neon_js_1.u.num2fixed8(transfer.amount), neon_js_1.u.str2hexstring(transfer.blockchainFrom), neon_js_1.u.str2hexstring(transfer.tx))).then(function (x) { return (console.log(x), console.log(x.tx && x.tx.scripts)); }).catch(_err_);
 }
 exports.sendNeoToken = sendNeoToken;
+function _err_(arg) {
+    console.log("error:");
+    console.error(arg);
+    if (arg && arg.result && arg.result.stack)
+        console.error(arg.result.stack);
+}
--- src/neo/neo.ts
+++ src/neo/neo.ts
@@ -1,7 +1,10 @@
+import Neon, { u as neon_u } from "@cityofzion/neon-js"
+
 import appConfig from "../config"
 
 import { get_last_transactions_by_address, getTxsWithLogs } from "./lib"
 import { parseExchangeCall, checkTxSuccess } from "./neo-vm"
+import { toScriptString, ctrInvoker, invoke, signRawTx } from "./tools"
 
 export let NEO_CONTRACT_ADDR = appConfig.neo.contract.addr
 
@@ -32,6 +35,9 @@ export function getNeoTransfers(callback: (err: any, transfers: ICrossExchangeTr
 						if (!contract || contract.method != "exchange")
 							return
 						
+						if (!log)
+							return console.log(`skipping tx #${tx.txid} without applog!`)
+						
 						if (!checkTxSuccess(log)) // failed transaction (shouldn't be in the blockchain anyway)
 							return
 						
@@ -57,4 +63,46 @@ export function sendNeoToken(transfer: ICrossExchangeTransfer)
 	console.log(`\n\n-----TRANSFER NEO-----\n`)
 	console.log(transfer)
 	console.log(`\n----------------------\n\n`)
+
+	let neoscan = `${appConfig.neo.apiUrl}/api/main_net`
+
+	let ctrHash = appConfig.neo.contract.hash
+
+	let makeScript = toScriptString(ctrHash)
+	let callContract = ctrInvoker(ctrHash, appConfig.neo.rpc)
+	let to = Neon.create.account(transfer.to)
+	console.log(transfer.to)
+	console.log(to.scriptHash)
+	callContract = (op: string, ...args: any[]) =>
+		invoke(
+			signRawTx(
+				appConfig.neo.contract.owner_pk,
+				makeScript(op, args)
+			).serialize(true),
+			appConfig.neo.rpc
+		)
+	function sendTx(script: string)
+	{
+		return Neon.doInvoke({
+			net: neoscan,
+			url: appConfig.neo.rpc,
+			script: script,
+			account: Neon.create.account(appConfig.neo.contract.owner_pk),
+			gas: 0.001,
+		}).then(tx => tx)
+	}
+	sendTx(makeScript('mintTokens',
+		neon_u.reverseHex(to.scriptHash),
+		neon_u.num2fixed8(transfer.amount),
+		neon_u.str2hexstring(transfer.blockchainFrom),
+		neon_u.str2hexstring(transfer.tx)
+	)).then(x => (console.log(x), console.log(x.tx && x.tx.scripts))).catch(_err_)
+}
+
+function _err_(arg: any)
+{
+	console.log(`error:`)
+	console.error(arg)
+	if (arg && arg.result && arg.result.stack)
+		console.error(arg.result.stack)
 }
