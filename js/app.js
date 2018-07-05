fdfa8c03fafc6d7c3bfeb17a06644390c2c683b4
zlumer <zlumer@gmail.com>
2018-07-05 18:42:41 +0300

    app: added testnet processed txs

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var tools_1 = require("./tools");
-var neo_1 = require("./neo/neo");
 var eth_1 = require("./eth/eth");
 var eos_1 = require("./eos/eos");
-var processed = { };
+var processed = {
+    'b1b0b7dc1a0b36bf8a2073f2406463aa65516deb131261462b8b7a36deb1f8f6': true,
+    'b2daa1fefd2ae1d0d3896525c9b9b0d07ac24b2547c43d6fb8ec9e35d3f1428a': true,
+    '3a95624ad301c3dcecd1401f87fae7d8b7d1ed887da7ae2f67f549fb668252f1': true,
+    "7794a22afda0750fc25ca05ed4d0442346d89595670a75ca7dfdb9dd365b7126": true,
+    "0": true,
+    "1": true,
+    "2": true,
+    "3": true,
+    "4": true,
+    "5": true,
+    "6": true,
+    "7": true,
+    "8": true,
+    // "1": true,
+    "0x74ab1dec2b138e8292edd33b3f82747968f9f8c70fa34889e181dbba932af4ed": true,
+    "0x9cdc1bab13ff6abbf47155e6a745024c52c4a18d834952728ee7ab54bd1d52ed": true,
+};
 _poll_();
 function _poll_() {
     console.log("poll " + new Date().toISOString());
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,24 @@ import { getNeoTransfers, sendNeoToken } from "./neo/neo"
 import { getEthTransfers, sendEthToken } from "./eth/eth"
 import { getEosTransfers, sendEosToken } from "./eos/eos"
 
-let processed: { [tx: string]: boolean } = { }
+let processed: { [tx: string]: boolean } = {
+	'b1b0b7dc1a0b36bf8a2073f2406463aa65516deb131261462b8b7a36deb1f8f6': true,
+	'b2daa1fefd2ae1d0d3896525c9b9b0d07ac24b2547c43d6fb8ec9e35d3f1428a': true,
+	'3a95624ad301c3dcecd1401f87fae7d8b7d1ed887da7ae2f67f549fb668252f1': true,
+	"7794a22afda0750fc25ca05ed4d0442346d89595670a75ca7dfdb9dd365b7126": true,
+	"0": true,
+	"1": true,
+	"2": true,
+	"3": true,
+	"4": true,
+	"5": true,
+	"6": true,
+	"7": true,
+	"8": true,
+	// "1": true,
+	"0x74ab1dec2b138e8292edd33b3f82747968f9f8c70fa34889e181dbba932af4ed": true,
+	"0x9cdc1bab13ff6abbf47155e6a745024c52c4a18d834952728ee7ab54bd1d52ed": true,
+}
 
 _poll_()
 
