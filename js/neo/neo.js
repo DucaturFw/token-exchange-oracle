12a60550eb8d2f799882369d1eb12948d390237a
zlumer <zlumer@gmail.com>
2018-07-09 00:05:32 +0300

    typescript compiler update

--- js/neo/neo.js
+++ js/neo/neo.js
@@ -1,9 +1,16 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var neon_js_1 = __importDefault(require("@cityofzion/neon-js"));
+var neon_js_1 = __importStar(require("@cityofzion/neon-js"));
 var config_1 = __importDefault(require("../config"));
 var lib_1 = require("./lib");
 var neo_vm_1 = require("./neo-vm");
--- js/neo/tools.js
+++ js/neo/tools.js
@@ -1,9 +1,13 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var neon_js_1 = __importDefault(require("@cityofzion/neon-js"));
+var neon_js_1 = __importStar(require("@cityofzion/neon-js"));
 var NET_NAME = 'PrivateNet';
 function initTestnet(_a) {
     var neoscan = _a.neoscan, neonDB = _a.neonDB;
